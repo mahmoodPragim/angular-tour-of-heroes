8cfd83eabd301351e7c4119fd33fd9a3ff86586d
Mahmood Ajawi <MahmoodA@leading-point.com>
2023-02-02 17:44:01 +0300

    remove unnecc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SiteComponent } from './site/site.component';
-import { Site2Component } from './site2/site2.component';
 
 const routes: Routes = [
-  {path:'site',component:SiteComponent},
-  {path:'site2',component:Site2Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SiteComponent } from './site/site.component';
-import { Site2Component } from './site2/site2.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    SiteComponent,
-    Site2Component
   ],
   imports: [
     BrowserModule,

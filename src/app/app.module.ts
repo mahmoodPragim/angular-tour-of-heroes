ca6a6b35e8fb1c783659601ac70acaa04721d65c
Mahmood Ajawi <MahmoodA@leading-point.com>
2023-02-02 19:17:19 +0300

    basic tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, TreeComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './angular-material.module';
 @NgModule({
   declarations: [
     AppComponent,
+    TreeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,

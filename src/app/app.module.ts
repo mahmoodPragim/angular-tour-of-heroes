06ade1cde34b591ef6e066bdc65d826bc92d85be
Mahmood Ajawi <MahmoodA@leading-point.com>
2023-02-03 22:56:36 +0300

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent, Services, TreeComponent } from './app.component';
+import { AppComponent, TreeComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './angular-material.module';
 import { HttpClientModule } from '@angular/common/http';
